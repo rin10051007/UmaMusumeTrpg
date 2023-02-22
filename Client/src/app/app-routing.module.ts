ec1a1b75f4d44adc27eca579f6527c7b84a4de70
RIN\Rinn1 <rin10071005@gmail.com>
2023-02-23 08:49:54 +0900

    ログインのComponentの作成

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'system', loadChildren: () => import('./system/system.module').then(m => m.SystemModule) }
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'system', loadChildren: () => import('./system/system.module').then(m => m.SystemModule) },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' }
 ];
 
 @NgModule({

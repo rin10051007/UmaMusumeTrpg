75b418966f6d521ac7caa320c0875221367133b3
RIN\Rinn1 <rin10071005@gmail.com>
2023-03-21 13:12:09 +0900

    Revert "developmentを削除"
    This reverts commit 9be39b53d5c7e434cf7d99a3e05dcba06037573f.

---
+++ Client/projects/common/src/lib/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://localhost:7058/',
+  authUrl: 'AuthControl',
+  systemUrl: 'SystemControl',
+  umaMusumeUrl: 'UmaMusumeControl'
+};

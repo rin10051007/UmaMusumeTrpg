9be39b53d5c7e434cf7d99a3e05dcba06037573f
RIN\Rinn1 <rin10071005@gmail.com>
2023-03-21 12:15:08 +0900

    developmentを削除

--- Client/projects/common/src/lib/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  baseUrl: 'https://localhost:7058/',
-  authUrl: 'AuthControl',
-  systemUrl: 'SystemControl',
-  umaMusumeUrl: 'UmaMusumeControl'
-};

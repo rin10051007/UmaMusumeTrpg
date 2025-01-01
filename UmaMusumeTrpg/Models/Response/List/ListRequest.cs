735040a0fd7a0d2f1c82fdc62a6fee339cc59ae0
RIN\Rinn1 <rin10071005@gmail.com>
2025-01-01 14:38:06 +0900

    スレッド、レス用のListAPIで使用する型を作成

--- UmaMusumeTrpg/Models/Response/List/ListRequest.cs
+++ UmaMusumeTrpg/Models/Response/List/ListRequest.cs
@@ -1,5 +1,18 @@
+using UmaMusumeTrpg.Models.Base.List;
+
 namespace UmaMusumeTrpg.Models.Response.List;
 
-public class ListRequest
+public class ListRequest(ListSearch search) : BaseListRequest(search)
 {
+    public new ListSearch Search { get; set; } = search;
+}
+
+public class ListRequestForThread(ListSearchForThread search) : ListRequest(search)
+{
+    public new ListSearchForThread Search { get; set; } = search;
+}
+
+public class ListRequestForUser(ListSearchForUser search) : ListRequest(search)
+{
+    public new ListSearchForUser Search { get; set; } = search;
 }
--- UmaMusumeTrpg/Models/User/List/ListResponse.cs
+++ UmaMusumeTrpg/Models/User/List/ListResponse.cs
@@ -2,9 +2,8 @@
 
 namespace UmaMusumeTrpg.Models.User.List;
 
-public class ListResponse(List<ListItem> items, int length, ListSearch search) : BaseListResponse([new BaseListItem(1)],
-    length,
-    search)
+public class ListResponse(List<ListItem> items, int length, ListSearch search)
+    : BaseListResponse(items.Cast<BaseListItem>().ToList(), length, search)
 {
     public new ListSearch Search { get; set; } = search;
     public new List<ListItem> Items { get; set; } = items;

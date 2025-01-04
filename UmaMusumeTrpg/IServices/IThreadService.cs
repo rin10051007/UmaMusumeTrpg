0b7d6e937adc812ce0fb26b63cb7839e3f2897e8
RIN\Rinn1 <rin10071005@gmail.com>
2025-01-04 10:22:08 +0900

    スレッドの各種一覧取得のServiceの作成

--- UmaMusumeTrpg/IServices/IThreadService.cs
+++ UmaMusumeTrpg/IServices/IThreadService.cs
@@ -1,5 +1,11 @@
+using UmaMusumeTrpg.Models.Thread.List;
+
 namespace UmaMusumeTrpg.IServices;
 
 public interface IThreadService
 {
+    public (List<ListItem>, int) GetList(ListSearch search);
+    public (List<ListItemForThread>, int) GetList(ListSearchForThread search);
+    public (List<ListItemForSystemThread>, int) GetList(ListSearchForSystemThread search);
+    public (List<ListItemForUser>, int) GetList(ListSearchForUser search);
 }
